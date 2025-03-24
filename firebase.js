fd9646b6baa442ce3f5a9641995999dab2842103
Shivpal Singh <157144680+RedChargeXT@users.noreply.github.com>
2025-03-25 01:36:45 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -3,20 +3,17 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebas
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
-// ✅ Firebase Configuration from `config.json`
-const response = await fetch('config.json');
-const config = await response.json();
 
+// ✅ Firebase Configuration from `.env`
 const firebaseConfig = {
-   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
     authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
     projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
     storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
     messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
     appId: import.meta.env.VITE_FIREBASE_APP_ID,
     measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
-
-};
+}
 
 // ✅ Initialize Firebase
 const app = initializeApp(firebaseConfig);
